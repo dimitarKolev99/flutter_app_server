3e09934da161c1eabd4de87bb72c6b108c1f3baa
DimitarKolev <s0568194@htw-berlin.de>
2022-02-13 11:47:03 +0100

    added cv webpage

---
+++ routes/api.js
@@ -0,0 +1,41 @@
+const express = require('express');
+const router = express.Router();
+const Product = require('../models/product');
+
+
+
+//get a list of ninjas from the db
+router.get('/product', function(req, res, next) {
+    Product.find({}).then(function(products) {
+        res.send(products);
+    });
+});
+
+//add a new ninja to the db
+router.post('/product', function(req, res, next) {
+    Product.create(req.body).then(function(product) {
+        res.send(product);
+    })
+    .catch(next);
+});
+
+//update a ninja in the db
+router.put('/ninjas/:id', function(req, res, next) {
+    Ninja.findByIdAndUpdate({ _id: req.params.id }, req.body)
+    .then(function(ninja) {
+        res.send(ninja);
+    })
+    .catch(next);
+});
+
+
+//delete a ninja from the db
+router.delete('/ninjas/:id', function(req, res, next) {
+    Ninja.findByIdAndRemove({ _id: req.params.id })
+        .then(function(ninja) {
+            res.send(ninja);
+        })
+        .catch(next);
+});
+
+module.exports = router;
--- server.js
+++ server.js
@@ -3,15 +3,35 @@ const express = require('express'),
       cookieParser = require('cookie-parser'),
       logger = require('morgan'),
       port = 3000,
-      app = express();
+      app = express(),
+      mongoose = require('mongoose');
+      require('dotenv').config();
+      
+//connect to mongodb
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then((result) => console.log('connected to db'))
+  .catch((err) => console.log(err));
+      
 
 app.use(logger('dev'));
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
+//init public folder
+app.use(express.static(path.join(__dirname, 'public')));
+
+//Init routes
+app.use('/api', require('./routes/api'));
+
+//default route
 app.get('/', function (req, res) { 
-  res.redirect('https://dimitarkolev99.github.io/#/');
+  res.sendFile(path.join(__dirname+'/cv.html'));
+});
+
+//error logging
+app.use(function (err, req, res, next) {
+  res.status(422).send({ error: err._message });
 });
 
 app.listen(process.env.PORT || port);
