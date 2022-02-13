0ee143243de9fae715aca43d284edc5c780679d9
DimitarKolev <s0568194@htw-berlin.de>
2022-02-13 10:16:41 +0100

    added redirect

--- server.js
+++ server.js
@@ -11,29 +11,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 
 app.get('/', function (req, res) { 
-  res.sendFile(path.join(__dirname, 'index.html'));
+  res.redirect('https://dimitarkolev99.github.io/#/');
 });
 
-module.exports = app;
-
-
-const download = (url, path, callback) => {
-  request.head(url, (err, res, body) => {
-    request(url)
-      .pipe(fs.createWriteStream(path))
-      .on('close', callback);
-  });
-};
-
-const url = 'https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg';
-
-const beforeDownload = +new Date;
-/* download(url, path, () => {
-    console.log('✅ Done!');
-    const afterDownload = +new Date;
-    console.log(afterDownload - beforeDownload);
-}); */
-
-
 app.listen(process.env.PORT || port);
-console.log(`Server listening on port: ${port}`);
