ae9cce182fc1f067e0c2f6e40538d1c6328c80c4
DimitarKolev <s0568194@htw-berlin.de>
2022-02-14 11:47:18 +0100

    improved cv design

--- server.js
+++ server.js
@@ -4,7 +4,10 @@ const express = require('express'),
       logger = require('morgan'),
       port = 3000,
       app = express(),
-      mongoose = require('mongoose');
+      mongoose = require('mongoose'),
+      WebSocket = require('ws'),
+      cors = require('cors');
+
       require('dotenv').config();
       
 //connect to mongodb
@@ -18,8 +21,25 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
+
+
 //init public folder
 app.use(express.static(path.join(__dirname, 'public')));
+
+//Config CORS
+app.use(cors());
+
+const a = Math.random();
+
+const wss = new WebSocket.Server({ server : app });
+
+wss.on('connection', function connection(ws, request, client) {
+   
+
+  ws.on('message', function message(data) {
+    console.log(`Received message ${data} from user ${client}`);
+  });
+});
 
 //Init routes
 app.use('/api', require('./routes/api'));
